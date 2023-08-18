404be4307d5f1dcee4b128f37819062d2a5d785a
Jacob Bryant <jacobryas4@gmail.com>
2023-08-18 15:56:35 -0400

    embedding youtube videos

--- src/components/FeatureCardMedia.tsx
+++ src/components/FeatureCardMedia.tsx
@@ -1,56 +1,81 @@
-
-
 interface CardProps {
   header: string;
   subHeader: string;
   imagePath?: string;
   videoPath?: string;
+  youtubeId?: string;
 }
 
 const styles = {
   card: {
-    height: 'auto', // Allow the height to adjust based on content
-    borderRadius: '16px',
-    backgroundColor: 'rgba(3, 16, 22, 1)',
-    border: '1px solid rgba(46, 140, 135, 1)',
-    padding: '32px',
+    height: "auto", // Allow the height to adjust based on content
+    borderRadius: "16px",
+    backgroundColor: "rgba(3, 16, 22, 1)",
+    border: "1px solid rgba(46, 140, 135, 1)",
+    padding: "32px",
   },
   img: {
-    borderRadius: '8px',
-    height: 'auto', // Allow the height to adjust based on the width
+    borderRadius: "8px",
+    height: "auto", // Allow the height to adjust based on the width
   },
   text: {
     header: {
-      fontSize: '28px',
-      fontWeight: '500',
+      fontSize: "28px",
+      fontWeight: "500",
     },
     subHeader: {
-      fontSize: '14px',
-      fontWeight: '400',
+      fontSize: "14px",
+      fontWeight: "400",
     },
   },
 };
 
-const Card = ({ header, subHeader, imagePath, videoPath }: CardProps) => {
+const Card = ({
+  header,
+  subHeader,
+  imagePath,
+  videoPath,
+  youtubeId,
+}: CardProps) => {
   return (
-    <div className="rounded-lg shadow-md flex flex-col md:flex-row justify-center text-left mx-auto my-5 w-5/6" style={styles.card}>
-      <div className="flex flex-col w-full md:w-2/4 text-center md:text-left my-0 md:my-auto px-3">
-        <h2 className="text-xl font-bold mt-0 mb-0 text-primary" style={styles.text.header}>
+    <div
+      className="rounded-lg shadow-md flex flex-col md:flex-row justify-center text-left mx-auto my-5 w-5/6"
+      style={styles.card}
+    >
+      <div className="flex flex-col w-full md:w-2/4 text-center md:text-left my-2 md:my-auto px-3">
+        <h2
+          className="text-xl font-bold mt-0 mb-0 text-primary"
+          style={styles.text.header}
+        >
           {header}
         </h2>
         <p className="text-white-600 mt-3" style={styles.text.subHeader}>
           {subHeader}
         </p>
       </div>
-      <div className="w-full md:w-2/4 border border-cacti-green rounded-md">
-        {imagePath && !videoPath && (
-          <img src={imagePath} alt="Card Image" className="w-full" style={styles.img} />
+      <div className="w-full md:w-2/4 h-auto border border-cacti-green p-1 rounded-md">
+        {imagePath && !videoPath && !youtubeId && (
+          <img
+            src={imagePath}
+            alt="Card Image"
+            className="w-full"
+            style={styles.img}
+          />
         )}
-        {videoPath && (
+        {videoPath && !youtubeId && (
           <video controls className="w-full" style={styles.img}>
             <source src={videoPath} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
+        )}
+        {youtubeId && (
+          <iframe
+            src={`https://www.youtube.com/embed/${youtubeId}`}
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            className="w-full aspect-video pa-1"
+          ></iframe>
         )}
       </div>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,47 +1,48 @@
-import TopBar from "@/components/TopBar"
-import { useRef, useEffect } from "react"
-import { Roboto_Mono } from 'next/font/google'
-import Image from 'next/image'
-import FeatureCardMedia from '@/components/FeatureCardMedia'
-import FeatureCardText from '@/components/FeatureCardText'
-import CactiFooter from "@/components/CactiFooter"
-import MailchimpForm from "@/components/MailChimpForm"
+import TopBar from "@/components/TopBar";
+import { useRef, useEffect } from "react";
+import { Roboto_Mono } from "next/font/google";
+import Image from "next/image";
+import FeatureCardMedia from "@/components/FeatureCardMedia";
+import FeatureCardText from "@/components/FeatureCardText";
+import CactiFooter from "@/components/CactiFooter";
+import MailchimpForm from "@/components/MailChimpForm";
 
-const robotoMono = Roboto_Mono({ subsets: ['latin'] })
+const robotoMono = Roboto_Mono({ subsets: ["latin"] });
 
 export default function Home() {
-
   const animationEl = useRef<HTMLSpanElement | null>(null);
-  const Typed = require('typed.js');
+  const Typed = require("typed.js");
 
   useEffect(() => {
-
     const typed = new Typed(animationEl.current, {
-      strings: ['Web3 Protocols','Smart Contracts', 'NFT Markets', 'Defi Protocols'],
+      strings: [
+        "Web3 Protocols",
+        "Smart Contracts",
+        "NFT Markets",
+        "Defi Protocols",
+      ],
       typeSpeed: 90,
-      loop: true
-    })
+      loop: true,
+    });
 
     return () => {
       typed.destroy();
-    }
-
-
-  }, [animationEl])
+    };
+  }, [animationEl]);
 
   return (
     <div>
       <header className={`${robotoMono.className}`}>
         <TopBar />
-
       </header>
       <main
-        className={`flex flex-col items-center overflow-scroll ${robotoMono.className} bg-cacti-dark-blue`}      
+        className={`flex flex-col items-center overflow-scroll ${robotoMono.className} bg-cacti-dark-blue`}
       >
-
         {/* Top Panel */}
-        <div className="flex flex-col items-center justify-center mt-0 border-cacti-green border-y-2 w-full mx-0 sm:min-h-[80h] lg:min-h-[50vh] xl:min-h-[50vh]" style={{ 
-          background: `linear-gradient(
+        <div
+          className="flex flex-col items-center justify-center mt-0 border-cacti-green border-y-2 w-full mx-0 sm:min-h-[80h] lg:min-h-[50vh] xl:min-h-[50vh]"
+          style={{
+            background: `linear-gradient(
             70deg,
             hsl(199deg 76% 5%) 0%,
             hsl(194deg 85% 7%) 16%,
@@ -54,63 +55,93 @@ export default function Home() {
             hsl(158deg 100% 16%) 92%,
             hsl(151deg 100% 17%) 100%
           )`,
-          // border: '1px solid rgba(46, 140, 135, 1)',
-        }}>
+            // border: '1px solid rgba(46, 140, 135, 1)',
+          }}
+        >
+          <div className="flex flex-col my-5 mt-12 px-3 md:px-0">
+            <div className="flex items-center text-center mx-auto">
+              <h1 className="text-3xl lg:text-6xl text-white">
+                🌵🌵🌵 CACTI 🌵🌵🌵
+              </h1>
+            </div>
 
-        <div className="flex flex-col my-5 mt-12 px-3 md:px-0">
-         
-
-          <div className="flex items-center text-center mx-auto">
-            <h1 className="text-4xl lg:text-6xl text-white">🌵🌵🌵 CACTI 🌵🌵🌵</h1>
-          </div>
-
-          <div className="text-md md:text-[22px] mt-5 text-center text-white">
-            Use Natural language to Interact with <div className="inline-block min-w-[215px] text-left">
-              <span ref={animationEl}>Web3 Protocols</span> 
+            <div className="text-md md:text-[22px] mt-5 text-center text-white">
+              Use Natural language to Interact with{" "}
+              <div className="inline-block min-w-[215px] text-left">
+                <span ref={animationEl}>Web3 Protocols</span>
               </div>
-          </div>
-          <div className="flex items-center justify-center text-sm md:text-[16px] my-5 text-center text-white">
-            Built by&nbsp;<a href="https://yieldprotocol.com/" target="_blank" rel="noreferrer" className="underline">Yield</a>&nbsp;<img src={'/icons/yield-logo-white.svg'} alt="icon" className="icon w-[16px]" />. Powered by OpenAI.
-          </div>
+            </div>
+            <div className="flex items-center justify-center text-sm md:text-[16px] my-5 text-center text-white">
+              Built by&nbsp;
+              <a
+                href="https://yieldprotocol.com/"
+                target="_blank"
+                rel="noreferrer"
+                className="underline"
+              >
+                Yield
+              </a>
+              &nbsp;
+              <img
+                src={"/icons/yield-logo-white.svg"}
+                alt="icon"
+                className="icon w-[16px]"
+              />
+              . Powered by OpenAI.
+            </div>
 
-          <div className="flex items-center w-full">
-
-            <MailchimpForm />
-            
+            <div className="flex items-center w-full">
+              <MailchimpForm />
+            </div>
+            <div className="flex items-center my-3">
+              <a
+                href="https://github.com/yieldprotocol/cacti-backend"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="mx-auto"
+              >
+                <button className="border border-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mx-auto">
+                  Show me the code 💻
+                </button>
+              </a>
+            </div>
           </div>
-          <div className="flex items-center my-3">
-            <a href="https://github.com/yieldprotocol/cacti-backend" target="_blank" rel="noopener noreferrer" className="mx-auto">
-              <button className="border border-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mx-auto">
-                Show me the code 💻
-              </button>
-            </a>
-          </div>
-
-        </div>
-          
         </div>
 
         {/* Panel 3 */}
         <div className="flex flex-col container items-center justify-center mx-auto mt-12 mb-1 px-3 md:px-0 bg-fade-black-to-transparent min-h-[40vh] text-primary">
-          <p className="text-lg md:text-3xl lg:text-5xl">Trade Assets. Retrieve Data.</p>
-          <p className="text-lg md:text-3xl lg:text-5xl">Interact with Smart Contracts.</p>
+          <p className="text-lg md:text-3xl lg:text-5xl">
+            Trade Assets. Retrieve Data.
+          </p>
+          <p className="text-lg md:text-3xl lg:text-5xl">
+            Interact with Smart Contracts.
+          </p>
           <div className="text-center pt-2">
-            <p className="text-sm md:text-lg text-white" style={{color: 'rgba(255, 255, 255, 0.8)'}}>The groundbreaking Cacti web-interface is in alpha development, with many protocols already integrated, and more on the way.</p>
+            <p
+              className="text-sm md:text-lg text-white"
+              style={{ color: "rgba(255, 255, 255, 0.8)" }}
+            >
+              The groundbreaking Cacti web-interface is in alpha development,
+              with many protocols already integrated, and more on the way.
+            </p>
           </div>
         </div>
 
         {/* Panel 2 */}
-        <div className="flex flex-col items-center justify-center container mx-auto h-full w-5/6 md:w-2/4 border border-cacti-green rounded-md py-1 mb-10" style={{}}>
-            <video controls height="auto">
-              <source src="/videos/yieldVideoHighRes.mp4" type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
+        <div
+          className="flex flex-col items-center justify-center container mx-auto h-full w-5/6 md:w-2/4 border border-cacti-green rounded-md p-1 mb-10"
+          style={{}}
+        >
+          <iframe
+            src="https://www.youtube.com/embed/cmTlxiU0r-Y"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            className="w-full aspect-video"
+          ></iframe>
         </div>
 
-
         {/* features */}
-        <div className="flex flex-col mx-auto h-full" style={{  }}>
-
+        <div className="flex flex-col mx-auto h-full" style={{}}>
           {/* Uniswap commented out until video */}
           {/* <div>
             <FeatureCardMedia 
@@ -124,7 +155,7 @@ export default function Home() {
             <FeatureCardMedia
               header="Search and Acquire NFTs with Opensea"
               subHeader="Search for NFTs by typing your request. Discover Collections and NFTs available for purchase and acquire them directly through Cacti."
-              videoPath="/videos/NFT-purchase-flow.mp4"
+              youtubeId="gPCMZ2b2BZg"
             />
           </div>
 
@@ -132,21 +163,27 @@ export default function Home() {
             <FeatureCardMedia
               header="Register and Interact with ENS Domains"
               subHeader="Effortlessly register and actively manage ENS domains to streamline your online presence."
-              videoPath="/videos/register-ens-and-set-avatar.mp4"
+              youtubeId="-qiqliXZBDw"
             />
           </div>
-
-
         </div>
 
         {/* Panel 4 */}
         <div className="flex flex-col items-center justify-center mx-auto min-h-[40vh] text-primary bg-fade-black-to-transparent">
-          <p className="text-lg md:text-3xl lg:text-5xl mb-5">Open source and built in Public</p>
+          <p className="text-lg md:text-3xl lg:text-5xl mb-5">
+            Open source and built in Public
+          </p>
           <div className="text-center flex flex-col">
-            <p className="text-sm md:text-lg text-[28px] mb-3 text-white" style={{color: 'rgba(255, 255, 255, 0.8)'}}>
+            <p
+              className="text-sm md:text-lg text-[28px] mb-3 text-white"
+              style={{ color: "rgba(255, 255, 255, 0.8)" }}
+            >
               We&apos;ve released Cacti under an AGPL license.
             </p>
-            <p className="text-sm md:text-lg text-[28px] text-white" style={{color: 'rgba(255, 255, 255, 0.8)'}}>
+            <p
+              className="text-sm md:text-lg text-[28px] text-white"
+              style={{ color: "rgba(255, 255, 255, 0.8)" }}
+            >
               Use our developer features and start building in the open with us.
             </p>
           </div>
@@ -154,51 +191,67 @@ export default function Home() {
 
         {/* Feature cards - no images */}
         <div className="container mx-auto px-12 py-8">
-
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          
             <div className="p-0">
-              <FeatureCardText header="Natural Language Interactions" subHeader="Allow users to trade assets, manage NFTs, and more through simple text requests and responses." />
+              <FeatureCardText
+                header="Natural Language Interactions"
+                subHeader="Allow users to trade assets, manage NFTs, and more through simple text requests and responses."
+              />
             </div>
             <div className="p-0">
-              <FeatureCardText header="Easy Integrations" subHeader="Seamlessly add web3 actions with Javascript on the frontend or Python on the backend." />
+              <FeatureCardText
+                header="Easy Integrations"
+                subHeader="Seamlessly add web3 actions with Javascript on the frontend or Python on the backend."
+              />
             </div>
             <div className="p-0">
-              <FeatureCardText header="ReactJS Component Framework" subHeader="Our library lets you build your UI quickly with minimal frontend knowledge." />
+              <FeatureCardText
+                header="ReactJS Component Framework"
+                subHeader="Our library lets you build your UI quickly with minimal frontend knowledge."
+              />
             </div>
             <div className="p-0">
-              <FeatureCardText header="Wallet based Authentication" subHeader="Ensure secure identification and transactions with user's wallets, powered by RainbowKit" />
+              <FeatureCardText
+                header="Wallet based Authentication"
+                subHeader="Ensure secure identification and transactions with user's wallets, powered by RainbowKit"
+              />
             </div>
             <div className="p-0">
-              <FeatureCardText header="Flexible Evaluation Framework" subHeader="Test the performance of widgets with hard-coded testing, automatic evaluations, or manually annotated CSV samples." />
+              <FeatureCardText
+                header="Flexible Evaluation Framework"
+                subHeader="Test the performance of widgets with hard-coded testing, automatic evaluations, or manually annotated CSV samples."
+              />
             </div>
             <div className="p-0">
-              <FeatureCardText header="Chat Context Management" subHeader="Build context from previous interactions for improved UX, and revisit past conversations with 1 click." />
+              <FeatureCardText
+                header="Chat Context Management"
+                subHeader="Build context from previous interactions for improved UX, and revisit past conversations with 1 click."
+              />
             </div>
-            
-            
           </div>
         </div>
 
-         {/* Panel 6 */}
-         <div className="flex flex-col items-center justify-center mx-auto text-primary bg-fade-black-to-transparent min-h-[40vh] w-5/6"  >
+        {/* Panel 6 */}
+        <div className="flex flex-col items-center justify-center mx-auto text-primary bg-fade-black-to-transparent min-h-[40vh] w-5/6">
           <p className="text-lg md:text-5xl mb-5">Come build with us</p>
           <div className="text-center flex flex-col">
             <div className="flex items-center">
-            <a href="https://github.com/yieldprotocol/cacti-backend" target="_blank" rel="noopener noreferrer">
-              <button className="border border-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mx-auto">
-                Show me the code 💻
-              </button>
-            </a>
+              <a
+                href="https://github.com/yieldprotocol/cacti-backend"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className="border border-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mx-auto">
+                  Show me the code 💻
+                </button>
+              </a>
             </div>
           </div>
         </div>
-        
-      {/* FOOTER */}
+
+        {/* FOOTER */}
         <CactiFooter />
       </main>
-
-
     </div>
-  )
+  );
 }
